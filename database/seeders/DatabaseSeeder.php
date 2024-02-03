0f2729869eaf234bd0753071217a6d09ae784815
Mostafa Mahfouz <MAHFOUZM25@GMAIL.COM>
2024-02-03 13:58:04 +0200

    database make all tables all releations in models add fake data for users and posts :

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
-
+        \App\Models\User::factory(10)->create()->each(function ($user) {
+            \App\Models\Post::factory(5)->create(['user_id' => $user->id]);
+        });
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
